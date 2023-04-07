b35ea8251677cfe08146f5a56740cd92d3e306a8
jhoves <2332689115@qq.com>
2023-04-07 23:58:41 +0800

    初始化

--- src/main/java/com/jhoves/ticket/TestController.java
+++ src/main/java/com/jhoves/ticket/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        return "Hello world1";
+        return "Hello world";
     }
 }
