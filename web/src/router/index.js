7ded7e746324785520d880e5ab5ae275e6a98eac
jhoves <2332689115@qq.com>
2023-04-12 10:33:42 +0800

    编写登录注册页面

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -10,10 +10,17 @@ const routes = [
   {
     path: '/about',
     name: 'about',
+    //懒加载
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    //懒加载
+    component: () => import('../views/login.vue')
   }
 ]
 
