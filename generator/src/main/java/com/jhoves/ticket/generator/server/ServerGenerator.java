281f95b61abf55b1d17ea7f88e68743067669fcd
jhoves <2332689115@qq.com>
2023-05-24 15:35:57 +0800

    集成dom4j,读取当前持久层的xml文件,得到表面和实体名

--- generator/src/main/java/com/jhoves/ticket/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jhoves/ticket/generator/server/ServerGenerator.java
@@ -3,6 +3,7 @@ package com.jhoves.ticket.generator.server;
 import com.jhoves.ticket.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
@@ -19,6 +20,20 @@ public class ServerGenerator {
     }
 
     public static void main(String[] args) throws Exception {
+        String generatorPath = getGeneratorPath();
+        Document document = new SAXReader().read("generator/" + generatorPath);
+        Node table = document.selectSingleNode("//table");
+        System.out.println(table);
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+//        FreemarkerUtil.initConfig("test.ftl");
+//        HashMap<String, Object> param = new HashMap<>();
+//        param.put("domain","Test");
+//        FreemarkerUtil.generator(toPath + "Test.java",param);
+    }
+
+    private static String getGeneratorPath() throws DocumentException {
         SAXReader saxReader = new SAXReader();
         HashMap<String, String> map = new HashMap<>();
         map.put("pom","http://maven.apache.org/POM/4.0.0");
@@ -26,10 +41,6 @@ public class ServerGenerator {
         Document document = saxReader.read(pomPath);
         Node node = document.selectSingleNode("//pom:configurationFile");
         System.out.println(node.getText());
-
-//        FreemarkerUtil.initConfig("test.ftl");
-//        HashMap<String, Object> param = new HashMap<>();
-//        param.put("domain","Test");
-//        FreemarkerUtil.generator(toPath + "Test.java",param);
+        return node.getText();
     }
 }
