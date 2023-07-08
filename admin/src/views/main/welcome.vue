cb449d380f4f3e259dab1744a400182dfd48b5cc
jhoves <2332689115@qq.com>
2023-07-08 16:29:38 +0800

    根据web模块来创建admin模块

---
+++ admin/src/views/main/welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>欢迎使用梦问票务系统</h1>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {
+    };
+  },
+});
+</script>
+<style>
+</style>
