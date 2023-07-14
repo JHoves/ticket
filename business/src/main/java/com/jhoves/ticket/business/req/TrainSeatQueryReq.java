ac808150d620f35c76c59aa4658b6c0190b64c48
jhoves <2332689115@qq.com>
2023-07-15 01:21:24 +0800

    为基础数据增加车次查询条件

--- business/src/main/java/com/jhoves/ticket/business/req/TrainSeatQueryReq.java
+++ business/src/main/java/com/jhoves/ticket/business/req/TrainSeatQueryReq.java
@@ -3,10 +3,21 @@ package com.jhoves.ticket.business.req;
 import com.jhoves.ticket.common.req.PageReq;
 
 public class TrainSeatQueryReq extends PageReq {
+    private String trainCode;
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
 
     @Override
     public String toString() {
         return "TrainSeatQueryReq{" +
-                "} " + super.toString();
+                "trainCode='" + trainCode + '\'' +
+                '}';
     }
+
 }
