44fb3a5928c62ffbbe5ef652c42bd56c93a0c3a1
jhoves <2332689115@qq.com>
2023-07-15 22:32:18 +0800

    使用数据库配置和编写接口来操作quartz定时任务

--- batch/src/main/java/com/jhoves/ticket/batch/config/QuartzConfig.java
+++ batch/src/main/java/com/jhoves/ticket/batch/config/QuartzConfig.java
@@ -1,36 +1,36 @@
-package com.jhoves.ticket.batch.config;
-
- import com.jhoves.ticket.batch.job.TestJob;
- import org.quartz.*;
- import org.springframework.context.annotation.Bean;
- import org.springframework.context.annotation.Configuration;
-
- @Configuration
- public class QuartzConfig {
-
-     /**
-      * 声明一个任务
-      * @return
-      */
-     @Bean
-     public JobDetail jobDetail() {
-         return JobBuilder.newJob(TestJob.class)
-                 .withIdentity("TestJob", "test")
-                 .storeDurably()
-                 .build();
-     }
-
-     /**
-      * 声明一个触发器，什么时候触发这个任务
-      * @return
-      */
-     @Bean
-     public Trigger trigger() {
-         return TriggerBuilder.newTrigger()
-                 .forJob(jobDetail())
-                 .withIdentity("trigger", "trigger")
-                 .startNow()
-                 .withSchedule(CronScheduleBuilder.cronSchedule("*/2 * * * * ?"))
-                 .build();
-     }
- }
+//package com.jhoves.ticket.batch.config;
+//
+// import com.jhoves.ticket.batch.job.TestJob;
+// import org.quartz.*;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// //自己手动声明的配置类（不经常使用）
+// @Configuration
+// public class QuartzConfig {
+//
+//     /**
+//      * 声明一个任务
+//      * @return
+//      */
+//     @Bean
+//     public JobDetail jobDetail() {
+//         return JobBuilder.newJob(TestJob.class)
+//                 .withIdentity("TestJob", "test")
+//                 .storeDurably()
+//                 .build();
+//     }
+//
+//     /**
+//      * 声明一个触发器，什么时候触发这个任务
+//      * @return
+//      */
+//     @Bean
+//     public Trigger trigger() {
+//         return TriggerBuilder.newTrigger()
+//                 .forJob(jobDetail())
+//                 .withIdentity("trigger", "trigger")
+//                 .startNow()
+//                 .withSchedule(CronScheduleBuilder.cronSchedule("*/2 * * * * ?"))
+//                 .build();
+//     }
+// }
