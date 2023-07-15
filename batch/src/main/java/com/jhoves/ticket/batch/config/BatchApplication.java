ed4edd2dd77c24960823a11c55e866f5592eb73b
jhoves <2332689115@qq.com>
2023-07-15 20:07:46 +0800

    创建batch模块并配置持久层生成器

---
+++ batch/src/main/java/com/jhoves/ticket/batch/config/BatchApplication.java
@@ -0,0 +1,25 @@
+package com.jhoves.ticket.batch.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+@ComponentScan("com.jhoves")
+@MapperScan("com.jhoves.ticket.*.mapper")
+public class BatchApplication {
+    //增加日志
+    private static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BatchApplication.class);
+        ConfigurableEnvironment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+    }
+
+}
