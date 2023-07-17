98149a1511fc7fd9e768d3001f365932e482f2ad
jhoves <2332689115@qq.com>
2023-07-17 21:35:32 +0800

    集成OpenFeign实现服务间调用

--- batch/src/main/java/com/jhoves/ticket/batch/config/BatchApplication.java
+++ batch/src/main/java/com/jhoves/ticket/batch/config/BatchApplication.java
@@ -5,12 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 @ComponentScan("com.jhoves")
 @MapperScan("com.jhoves.ticket.*.mapper")
+@EnableFeignClients("com.jhoves.ticket.batch.feign")
 public class BatchApplication {
     //增加日志
     private static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
--- batch/src/main/java/com/jhoves/ticket/batch/controller/TestController.java
+++ batch/src/main/java/com/jhoves/ticket/batch/controller/TestController.java
@@ -1,12 +1,24 @@
 package com.jhoves.ticket.batch.controller;
 
+import com.jhoves.ticket.batch.feign.BusinessFeign;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Resource
+    BusinessFeign businessFeign;
+
     @GetMapping("/test")
-    public String hello(){
-        return "test batch";
+    public String hello() {
+        String businessHello = businessFeign.hello();
+        LOG.info(businessHello);
+        return "Hello World! Batch! " + businessHello;
     }
 }
