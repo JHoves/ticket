41874795877f5262ad78cc97f7a7ac676f92127c
jhoves <2332689115@qq.com>
2023-07-26 23:09:15 +0800

    计算多个选座之间的偏移值

--- business/src/main/java/com/jhoves/ticket/business/service/ConfirmOrderService.java
+++ business/src/main/java/com/jhoves/ticket/business/service/ConfirmOrderService.java
@@ -4,11 +4,13 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.EnumUtil;
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.jhoves.ticket.business.domain.DailyTrainTicket;
 import com.jhoves.ticket.business.enums.ConfirmOrderStatusEnum;
+import com.jhoves.ticket.business.enums.SeatColEnum;
 import com.jhoves.ticket.business.enums.SeatTypeEnum;
 import com.jhoves.ticket.business.req.ConfirmOrderDoReq;
 import com.jhoves.ticket.business.req.ConfirmOrderTicketReq;
@@ -27,6 +29,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -96,6 +99,7 @@ public class ConfirmOrderService {
         String trainCode = req.getTrainCode();
         String start = req.getStart();
         String end = req.getEnd();
+        List<ConfirmOrderTicketReq> tickets = req.getTickets();
         ConfirmOrder confirmOrder = new ConfirmOrder();
         confirmOrder.setId(SnowUtil.getSnowflakeNextId());
         confirmOrder.setCreateTime(now);
@@ -107,7 +111,7 @@ public class ConfirmOrderService {
         confirmOrder.setEnd(end);
         confirmOrder.setDailyTrainTicketId(req.getDailyTrainTicketId());
         confirmOrder.setStatus(ConfirmOrderStatusEnum.INIT.getCode());
-        confirmOrder.setTickets(JSON.toJSONString(req.getTickets()));
+        confirmOrder.setTickets(JSON.toJSONString(tickets));
 
         confirmOrderMapper.insert(confirmOrder);
 
@@ -117,6 +121,44 @@ public class ConfirmOrderService {
 
         //扣减余票数量，并判断余票是否足够
         reduceTickets(req, dailyTrainTicket);
+
+        //难点:↓
+        //计算相对第一个座位的偏离值
+        //比如选择的是C1，D2，则偏移值是：[0,5]
+        //比如选择的是A1，B1，C1，则偏移值是：[0，1，2]
+        ConfirmOrderTicketReq ticketReq0 = tickets.get(0);
+        if(StrUtil.isNotBlank(ticketReq0.getSeat())){
+            LOG.info("本次购票有选座");
+            //查出本次选座的座位类型都有那些列，用于计算所选座位与第一个座位的偏离值
+            List<SeatColEnum> colEnumList = SeatColEnum.getColsByType(ticketReq0.getSeatTypeCode());
+            LOG.info("本次选座的座位类型包含的列：{}",colEnumList);
+
+            //组成和前端两排选座一样的列表，用于做参照的座位列表，例：referSeatList = {A1,C1,D1,F1,A2,C2,D2,F2}
+            ArrayList<Object> referSeatList = new ArrayList<>();
+            for(int i = 1;i <= 2; i++){
+                for (SeatColEnum seatColEnum : colEnumList) {
+                    referSeatList.add(seatColEnum.getCode() + i);
+                }
+            }
+            LOG.info("用于做参照的两排座位：{}",referSeatList);
+
+            List<Integer> offsetList = new ArrayList<>();
+            //绝对偏移值，即：在参照座位列表中的位置
+            List<Integer> aboluteOffsetList = new ArrayList<>();
+            for(ConfirmOrderTicketReq ticketReq : tickets) {
+                int index = referSeatList.indexOf(ticketReq.getSeat());
+                aboluteOffsetList.add(index);
+            }
+            LOG.info("计算得到所有座位的绝对值偏移值：{}",aboluteOffsetList);
+            for(Integer index : aboluteOffsetList) {
+                int offset = index - aboluteOffsetList.get(0);
+                offsetList.add(offset);
+            }
+            LOG.info("计算得到所有座位的相对第一个座位的偏移值：{}",offsetList);
+
+        }else{
+            LOG.info("本次购票没有选座");
+        }
 
 
         //选座
@@ -138,7 +180,6 @@ public class ConfirmOrderService {
             switch (seatTypeEnum){
                 case YDZ:
                     int YDZCountLeft = dailyTrainTicket.getYdz() - 1;
-                    LOG.info("查出YDZ余票记录:{}",YDZCountLeft);
                     if(YDZCountLeft < 0){
                         throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
                     }
