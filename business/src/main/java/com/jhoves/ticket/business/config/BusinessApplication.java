729f8204e830d7f1a04234729fff7d97278d366c
jhoves <2332689115@qq.com>
2023-08-05 23:17:34 +0800

    使用Sentinel配置和控台进行限流

--- business/src/main/java/com/jhoves/ticket/business/config/BusinessApplication.java
+++ business/src/main/java/com/jhoves/ticket/business/config/BusinessApplication.java
@@ -1,5 +1,9 @@
 package com.jhoves.ticket.business.config;
 
+import com.alibaba.csp.sentinel.slots.block.Rule;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +13,9 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @ComponentScan("com.jhoves")
@@ -24,6 +31,23 @@ public class BusinessApplication {
         ConfigurableEnvironment env = app.run(args).getEnvironment();
         LOG.info("启动成功！！");
         LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+
+        //限流规则
+        //直接控制进行设置规则了，所以不用了
+//        initFlowRules();
+//        LOG.info("限流规则");
+    }
+
+    public static void initFlowRules(){
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("doConfirm");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        //set limit qps to 1
+        rule.setCount(20);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+
     }
 
 }
--- business/src/main/java/com/jhoves/ticket/business/service/ConfirmOrderService.java
+++ business/src/main/java/com/jhoves/ticket/business/service/ConfirmOrderService.java
@@ -8,6 +8,8 @@ import cn.hutool.core.util.EnumUtil;
 import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -111,6 +113,8 @@ public class ConfirmOrderService {
         confirmOrderMapper.deleteByPrimaryKey(id);
     }
 
+    //@SentinelResource("doConfirm") // 限流熔断的接口
+    @SentinelResource(value = "doConfirm",blockHandler = "doConfirmBlock")
     public void doConfirm(ConfirmOrderDoReq req) {
         //分布式锁解决超卖问题
         String lockKey = DateUtil.formatDate(req.getDate()) + "-" + req.getTrainCode();
@@ -438,5 +442,15 @@ public class ConfirmOrderService {
                     break;
             }
         }
+    }
+
+    /**
+     * 降级方法，需包含限流方法的所有参数和BlockException参数
+     * @param req
+     * @param e
+     */
+    public void doConfirmBlock(ConfirmOrderDoReq req, BlockException e) {
+        LOG.info("购票请求被限流：{}", req);
+        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION);
     }
 }
